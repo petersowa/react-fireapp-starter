8913aa21833e3e24dd982284621630b78ed328a5
petersowa <petersowa@users.noreply.github.com>
2019-11-09 09:19:19 -0600

    update layout and styles

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Header = () => <header className="header">Page Title - Header</header>;
 const Footer = () => <footer className="footer">Page Footer - Author</footer>;
 const Main = ({ children }) => (
 	<main className="main container">{children}</main>
 );
+Main.propTypes = {
+	children: PropTypes.node.isRequired,
+};
 
 const Article = () => (
-	<article>
-		<p>Article Text</p>
-	</article>
-);
-
-const Section = () => (
-	<section className="content-section">
+	<article className="article">
 		<h1>Hello World.</h1>
 		<hr></hr>
 		<h2>The is a boiler plate react app</h2>
@@ -42,18 +40,43 @@ const Section = () => (
 			vitae quos voluptas? Enim quae tempore officiis nobis laudantium
 			recusandae minima?
 		</p>
+	</article>
+);
+
+const Section = () => (
+	<section className="content-section">
+		<Article />
 		<button className="comment-button">Comment</button>
 	</section>
 );
+
+const Sidebar = () => (
+	<aside className="sidebar">
+		<nav>
+			<ul>
+				<li>App</li>
+				<li>Profile</li>
+				<li>About</li>
+				<li>Contact</li>
+			</ul>
+		</nav>
+	</aside>
+);
+
+const Layout = ({ children }) => <div className="layout">{children}</div>;
 
 const App = () => {
 	return (
 		<div className="app-content">
 			<Header></Header>
-			<Main>
-				<Section></Section>
-				<Section></Section>
-			</Main>
+			<Layout>
+				<Sidebar />
+				<Main>
+					<Section></Section>
+					<Section></Section>
+					<Section></Section>
+				</Main>
+			</Layout>
 			<Footer></Footer>
 		</div>
 	);
